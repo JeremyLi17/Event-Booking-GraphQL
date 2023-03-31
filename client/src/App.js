e6a68418b4a3a35745ce0a1eced2f5a45dbaa878
JeremyLi17 <HaotianLi0505@hotmail.com>
2023-03-31 00:31:26 -0400

    main navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
-import './App.css';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
+import './App.css';
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/events" Component={EventsPage} />
-          <Route path="/bookings" Component={BookingsPage} />
-          <Route path="/" element={<Navigate replace={true} to="/auth" />} />
-        </Routes>
+        <MainNavigation />
+        <main className="main-content">
+          <Routes>
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/events" Component={EventsPage} />
+            <Route path="/bookings" Component={BookingsPage} />
+            <Route path="/" element={<Navigate replace={true} to="/auth" />} />
+          </Routes>
+        </main>
       </BrowserRouter>
     </div>
   );
