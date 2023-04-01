8cfd15f0b4214871a23ab89084ebd685cad1f7df
JeremyLi17 <HaotianLi0505@hotmail.com>
2023-03-31 22:52:03 -0400

    backdrop component

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
 import './Events.css';
 
 const EventsPage = () => {
+  const [creating, setCreating] = useState(false);
+
+  const modalConfirm = () => {
+    setCreating(false);
+  };
+
+  const modalCancel = () => {
+    setCreating(false);
+  };
+
   return (
     <>
-      <Modal title="Add Event" canCancel canConfirm>
-        <p>Modal Content</p>
-      </Modal>
+      {creating && <Backdrop />}
+      {creating && (
+        <Modal
+          title="Add Event"
+          canCancel
+          canConfirm
+          onConfirm={modalConfirm}
+          onCancel={modalCancel}
+        >
+          <p>Modal Content</p>
+        </Modal>
+      )}
       <div className="events-control">
         <p>Share your own Events!</p>
-        <button className="btn">Create Event</button>
+        <button className="btn" onClick={() => setCreating(true)}>
+          Create Event
+        </button>
       </div>
     </>
   );
