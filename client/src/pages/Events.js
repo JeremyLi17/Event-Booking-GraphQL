5dfff576be578dba6ae6ffeb12b8a24eb79dd15d
JeremyLi17 <HaotianLi0505@hotmail.com>
2023-03-31 22:29:51 -0400

    add Event Modal

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import Modal from '../components/Modal/Modal';
+import './Events.css';
+
 const EventsPage = () => {
-  return <h1>The Events Page</h1>;
+  return (
+    <>
+      <Modal title="Add Event" canCancel canConfirm>
+        <p>Modal Content</p>
+      </Modal>
+      <div className="events-control">
+        <p>Share your own Events!</p>
+        <button className="btn">Create Event</button>
+      </div>
+    </>
+  );
 };
 
 export default EventsPage;
